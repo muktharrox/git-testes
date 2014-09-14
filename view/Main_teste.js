1945f8614ac51d660177f938860a4643b976a048
muktharrox <helio.afsilva@gmail.com>
2014-09-14 16:05:12 -0300

    Adicionado Main teste

---
+++ view/Main_teste.js
@@ -0,0 +1,32 @@
+//Esse e o meu arquivo principal
+Ext.define('Tocadobicho.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video',
+        'Ext.Map'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                xtype:'home'
+            },
+            {
+                xtype:'horario'
+            },
+            {
+                xtype:'localizacao'
+            },
+            {
+                xtype:'veterinario'
+            },
+            {
+                xtype:'loja'
+            }
+        ]
+    }
+});
